dbbe5778312fe2eed19adb991072fc4af70f85de
urvisha bambhroliya <bambhroliyaurvisha79@gmail.com>
2024-05-16 20:26:31 +0530

    country data project submitted

---
+++ assignmenthtml/React/countrydata/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* https://restcountries.com/v3.1/all */}
+      <Header/>
+      
+     </div>
+  );
+}
+
+export default App;
---
+++ assignmenthtml/React/countrydata/src/Header.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from "react";
+import "./Header.css";
+import CountryDeatils from "./CountryDeatils";
+
+const Header = () => {
+  // const getData = ()=>
+
+  const [data, setData] = useState(null);
+  const [random, setRandom] = useState(0);
+
+  const selectCountry = () => {
+    const randomIndex = Math.floor(Math.random() * 250);
+    setRandom(randomIndex);
+  };
+
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+        console.log(data);
+      });
+  }, []);
+
+  if (!data) {
+    return <></>;
+  }
+
+  return (
+    <div>
+      <CountryDeatils country={data[random]} />
+      {/* <div className="box">
+        <div className="img">Image</div>
+        <div className="name">name</div>
+        <div className="details">Capital :{data[random].capital[0]}</div>
+        <div className="details">Language : </div>
+        <div className="details">Population : </div>
+        <div className="details">Currency : </div>
+      </div> */}
+      <button
+        onClick={() => {
+          selectCountry();
+        }}
+      >
+        Select Country
+      </button>
+    </div>
+  );
+};
+
+export default Header;
